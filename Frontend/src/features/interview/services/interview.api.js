212ade4c6f16642a6009411bb8ac8f2602de0c2b
GAURAV CHAUBEY <gauravchaubey292003@gmail.com>
2026-04-12 13:14:38 +0530

    Update interview.api.js

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: https://genai-resume-backend-kxru.onrender.com,
+    baseURL: "https://genai-resume-backend-kxru.onrender.com",
     withCredentials: true,
 })
 
